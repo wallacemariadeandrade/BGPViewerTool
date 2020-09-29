4701f76909dbb21bcc7fe84eb701357f11f13adc
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2020-09-29 10:51:19 -0300

    Listing IPv6 address at get_asn_prefixes script

--- BGPViewerPowerTool/get_asn_prefixes.ps1
+++ BGPViewerPowerTool/get_asn_prefixes.ps1
@@ -6,9 +6,15 @@ $asn = $args[0]
 # Call the API
 $model = RetrieveJsonFrom("https://api.bgpview.io/asn/$($asn)/prefixes")
 # Extract from JSON
-Write-Host "=== ASN$($asn) Known Prefixes ==="
+Write-Host "=== ASN$($asn) Known IPv4 Prefixes ==="
 Write-Host ""
 foreach ($address in $model.data.ipv4_prefixes) {
+    Write-Host $address.prefix
+}
+Write-Host ""
+Write-Host "=== ASN$($asn) Known IPv6 Prefixes ==="
+Write-Host ""
+foreach ($address in $model.data.ipv6_prefixes) {
     Write-Host $address.prefix
 }
 Write-Host ""
