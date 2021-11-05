6ead9e4e752a1acb97ace085265ddacb3600bf0f
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2021-11-05 09:02:22 -0300

    Implements async operations and do some refactorings

--- BGPViewerCore/Service/BGPViewerWebApi.cs
+++ BGPViewerCore/Service/BGPViewerWebApi.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -6,78 +5,47 @@ namespace BGPViewerCore.Service
 {
     public class BGPViewerWebApi : IBGPViewerApi
     {
-        private const string BASE_ASN_URL = "https://api.bgpview.io/asn/";
-        public JsonDocument RetrieveAsnDetails(int asNumber)
-            => JsonDocument.Parse(CallApi($"{BASE_ASN_URL}{asNumber}"));
+        #region ENDPOINTS BUILDERS
+        private const string BASE_ENDPOINT_URL = "https://api.bgpview.io";
+        private string BuildAsnDetailsEndpoint(int asNumber) => $"{BASE_ENDPOINT_URL}/asn/{asNumber}";
+        private string BuildAsnDownstreamsEndpoint(int asNumber) => $"{BuildAsnDetailsEndpoint(asNumber)}/downstreams";
+        private string BuildAsnIxsEndpoint(int asNumber) => $"{BuildAsnDetailsEndpoint(asNumber)}/ixs";
+        private string BuildAsnPeersEndpoint(int asNumber) => $"{BuildAsnDetailsEndpoint(asNumber)}/peers";
+        private string BuildAsnPrefixesEndpoint(int asNumber) => $"{BuildAsnDetailsEndpoint(asNumber)}/prefixes";
+        private string BuildAsnUpstreamsEndpoint(int asNumber) => $"{BuildAsnDetailsEndpoint(asNumber)}/upstreams";
+        private string BuildIpDetailsEndpoint(string ipAddress) => $"{BASE_ENDPOINT_URL}/ip/{ipAddress}";
+        private string BuildPrefixDetailsEndpoint(string prefix, byte cidr) => $"{BASE_ENDPOINT_URL}/prefix/{prefix}/{cidr}";
+        private string BuildSearchByEndpoint(string queryTerm) => $"{BASE_ENDPOINT_URL}/search?query_term={queryTerm}";
+        #endregion
 
-        public async Task<JsonDocument> RetrieveAsnDetailsAsync(int asNumber)
-            => await JsonDocument.ParseAsync(await WebService.GetContentStreamFromAsync($"{BASE_ASN_URL}{asNumber}"));
+        #region INTERNAL OPERATIONS
+        private JsonDocument ParseOperation(string url) => JsonDocument.Parse(WebService.GetContentFrom(url));
+        private async Task<JsonDocument> ParseOperationAsync(string url) 
+            => await JsonDocument.ParseAsync(await WebService.GetContentStreamFromAsync(url));
+        #endregion
 
-        public JsonDocument RetrieveAsnDownstreams(int asNumber)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/asn/{asNumber}/downstreams"));
+        #region SYNC APIs
+        public JsonDocument RetrieveAsnDetails(int asNumber) => ParseOperation(BuildAsnDetailsEndpoint(asNumber));
+        public JsonDocument RetrieveAsnDownstreams(int asNumber) => ParseOperation(BuildAsnDownstreamsEndpoint(asNumber));
+        public JsonDocument RetrieveAsnIxs(int asNumber) => ParseOperation(BuildAsnIxsEndpoint(asNumber));
+        public JsonDocument RetrieveAsnPeers(int asNumber) => ParseOperation(BuildAsnPeersEndpoint(asNumber));
+        public JsonDocument RetrieveAsnPrefixes(int asNumber) => ParseOperation(BuildAsnPrefixesEndpoint(asNumber));
+        public JsonDocument RetrieveAsnUpstreams(int asNumber) => ParseOperation(BuildAsnUpstreamsEndpoint(asNumber));
+        public JsonDocument RetrieveIpDetails(string ipAddress) => ParseOperation(BuildIpDetailsEndpoint(ipAddress));
+        public JsonDocument RetrievePrefixDetails(string prefix, byte cidr) => ParseOperation(BuildPrefixDetailsEndpoint(prefix, cidr));
+        public JsonDocument RetrieveSearchBy(string queryTerm) => ParseOperation(BuildSearchByEndpoint(queryTerm));
+        #endregion
 
-        public Task<JsonDocument> RetrieveAsnDownstreamsAsync(int asNumber)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public JsonDocument RetrieveAsnIxs(int asNumber)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/asn/{asNumber}/ixs"));
-
-        public Task<JsonDocument> RetrieveAsnIxsAsync(int asNumber)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public JsonDocument RetrieveAsnPeers(int asNumber)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/asn/{asNumber}/peers"));
-
-        public Task<JsonDocument> RetrieveAsnPeersAsync(int asNumber)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public JsonDocument RetrieveAsnPrefixes(int asNumber)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/asn/{asNumber}/prefixes"));
-
-        public Task<JsonDocument> RetrieveAsnPrefixesAsync(int asNumber)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public JsonDocument RetrieveAsnUpstreams(int asNumber)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/asn/{asNumber}/upstreams"));
-
-        public Task<JsonDocument> RetrieveAsnUpstreamsAsync(int asNumber)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public JsonDocument RetrieveIpDetails(string ipAddress)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/ip/{ipAddress}"));
-
-        public Task<JsonDocument> RetrieveIpDetailsAsync(string ipAddress)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public JsonDocument RetrievePrefixDetails(string prefix, byte cidr)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/prefix/{prefix}/{cidr}"));
-
-        public Task<JsonDocument> RetrievePrefixDetailsAsync(string prefix, byte cidr)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public JsonDocument RetrieveSearchBy(string queryTerm)
-            => JsonDocument.Parse(CallApi($"https://api.bgpview.io/search?query_term={queryTerm}"));
-
-        public Task<JsonDocument> RetrieveSearchByAsync(string queryTerm)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private string CallApi(string url) => WebService.GetContentFrom(url);
-        private Task<Stream> CallApiAsync(string url) => WebService.GetContentStreamFromAsync(url);
+        #region ASYNC APIs
+        public async Task<JsonDocument> RetrieveAsnDetailsAsync(int asNumber) => await ParseOperationAsync(BuildAsnDetailsEndpoint(asNumber));
+        public async Task<JsonDocument> RetrieveAsnDownstreamsAsync(int asNumber) => await ParseOperationAsync(BuildAsnDownstreamsEndpoint(asNumber));
+        public async Task<JsonDocument> RetrieveAsnIxsAsync(int asNumber) => await ParseOperationAsync(BuildAsnIxsEndpoint(asNumber));
+        public async Task<JsonDocument> RetrieveAsnPeersAsync(int asNumber) => await ParseOperationAsync(BuildAsnPeersEndpoint(asNumber));
+        public async Task<JsonDocument> RetrieveAsnPrefixesAsync(int asNumber) => await ParseOperationAsync(BuildAsnPrefixesEndpoint(asNumber));
+        public async Task<JsonDocument> RetrieveAsnUpstreamsAsync(int asNumber) => await ParseOperationAsync(BuildAsnUpstreamsEndpoint(asNumber));        
+        public async Task<JsonDocument> RetrieveIpDetailsAsync(string ipAddress) => await ParseOperationAsync(BuildIpDetailsEndpoint(ipAddress));
+        public async Task<JsonDocument> RetrievePrefixDetailsAsync(string prefix, byte cidr) => await ParseOperationAsync(BuildPrefixDetailsEndpoint(prefix, cidr));
+        public async Task<JsonDocument> RetrieveSearchByAsync(string queryTerm) => await ParseOperationAsync(BuildSearchByEndpoint(queryTerm));
+        #endregion
     }
 }
